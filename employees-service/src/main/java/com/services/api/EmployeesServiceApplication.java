6e43d3b0b21bf17d0b4a6cff8c613da98adf36e7
Mohamed_Jar <jarraymohamed92@hotmail.fr>
2019-06-23 21:40:18 +0200

    [feat] add config for employees service + H2 data base

--- employees-service/src/main/java/com/services/api/EmployeesServiceApplication.java
+++ employees-service/src/main/java/com/services/api/EmployeesServiceApplication.java
@@ -1,18 +1,19 @@
 package com.services.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableSwagger2
 public class EmployeesServiceApplication {
 
@@ -27,11 +28,15 @@ public class EmployeesServiceApplication {
 				.apis(RequestHandlerSelectors.basePackage("com.services.api.v1.employees.controllers"))
 				.paths(PathSelectors.any())
 				.build()
-				.apiInfo(new ApiInfoBuilder()
-						.version("1.0")
-						.title("Employees")
-						.description("Documentation Employees API v1.0")
-						.build());
+				.apiInfo(apiInfoBuilder());
+	}
+
+	private ApiInfo apiInfoBuilder() {
+		return new ApiInfoBuilder()
+				.version("1.0")
+				.title("Employees")
+				.description("Documentation Employees API v1.0")
+				.build();
 	}
 
 }
